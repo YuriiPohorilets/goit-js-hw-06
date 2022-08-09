e3a8ffc04b644ae5cca62b74b2924e81bcf805ce
YuriiPohorilets <pohorilets.y@gmail.com>
2022-08-09 20:43:28 +0300

    task #8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === '' || password.value === '') {
+    alert('All fields must be filled!');
+  } else {
+    const userObj = {
+      email: email.value,
+      password: password.value,
+    };
+
+    event.currentTarget.reset();
+
+    console.log(userObj);
+  }
+}
+
+formRef.addEventListener('submit', onFormSubmit);
