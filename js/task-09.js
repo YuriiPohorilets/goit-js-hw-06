637e38a31044e2b08cf97df247b16e0b31f0da62
YuriiPohorilets <pohorilets.y@gmail.com>
2022-08-09 20:54:36 +0300

    task #9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const outputColor = document.querySelector('.color');
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  outputColor.textContent = getRandomHexColor();
+}
+
+btnChangeColor.addEventListener('click', onChangeColor);
